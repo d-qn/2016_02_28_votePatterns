e69258267fcd8b152e712a66340f58c32dfa2879
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2016-02-17 17:28:45 +0100

    slopegraph populist vote ready

--- 03_slopegraph_populist.R
+++ 03_slopegraph_populist.R
@@ -1,7 +1,9 @@
-#source("~/swissinfo/_helpers/helpers.R")
 library(swiTheme)
 library(swiMap)
 library(slopegraph)
+library(dplyr)
+library(htmltools)
+library(svglite)
 
 ############################################################################################
 ###   load vote data
@@ -9,15 +11,14 @@ library(slopegraph)
 
 font <- "Open Sans"
 # load results for the whole of Switzerland
-voteCH <- read.csv("~/swissinfo/_helpers/cantonal/vote/genetic/concat_genetic.csv", row.names = 1, check.names = F)
+voteCH <- read.csv("~/swissinfo/_helpers/cantonal/vote/populist/concatenate_popu.csv", row.names = 1, check.names = F)
 colnames(voteCH) <- gsub("\\.", " ", colnames(voteCH))
 
 # load the latest genetic vote results
-
 vote2 <- read.csv("data/reproductive_medicine.csv", check.names = F)[,-3]
 stopifnot(canton_namesStrict(rownames(voteCH), 'iso2') == vote2[,1] )
 
-voteCH <- cbind(voteCH, dpi = round(vote2[,2], 1))
+voteCH <- cbind(voteCH, dsi = round(vote2[,2], 1))
 
 # TRANSFORM 2nd vote into % of no
 voteCH[,2] <- 100 - voteCH[,2]
@@ -38,35 +39,46 @@ indicators <- do.call(cbind, lapply(ifiles, function (csv) {
 pop <- indicators[,which(colnames(indicators) == 'Population.Habitants.en.milliers'), drop = FALSE]
 
 
-df <- voteCH
+#df <- voteCH
+### Drop some votes!
+df <- voteCH %>% select(-`Contre la construction de minarets (29 11 2009)`, -`Contre l'immigration de masse (09 2 2014)`, -`Ecopop (30 11 2014)`)
 
 
 lineW <- pop[match(canton_namesStrict(rownames(df)), rownames(pop)),1]
 lineW <- c(lineW,  max(lineW) + mean(lineW))
 cols <- c(rep("black", nrow(voteCH)), swi_pal[9])
 
-pdfswi_long("genetic_slopegraph.pdf")
+pdfswi_long("populist_slopegraph.pdf")
 par(family = font)
 slopegraph(df, rescaleByColumn = F, cex.lab = 0.5, cex.num = 0.6, lab.sep = 0.25,
 	offset.x = 0.08, offset.lab = 0.03, xlim = c(-1, ncol(voteCH) + 1.2),
 	collapse.label = ", ", lwd =  scale(lineW, center = F), col.lines = cols, mai = c(0, 0, 0.6, 0),
 	main = "")
-dev.off()
-
-
 
 par(family = font)
 slopegraph(df, rescaleByColumn = F, cex.lab = 0.5, cex.num = 0.6, lab.sep = 0.25,
 	offset.x = 0.045, offset.lab = 0.03, xlim = c(-1.2, ncol(votes) + 1.2),
 	collapse.label = ", ", lwd =  scale(lineW, center = F) / 2, col.lines = cols, mai = c(0.01, 0, 0.9, 0))
-
-
-par(family = font)
-slopegraph(df[,2:4], rescaleByColumn = F, cex.lab = 0.5, cex.num = 0.6, lab.sep = 0.25,
-	offset.x = 0.07, offset.lab = 0.03, xlim = c(-0.6, ncol(votes) + 0.8),
-	collapse.label = ", ", lwd =  scale(lineW, center = F) / 2, col.lines = cols, mai = c(0.01, 0, 0.9, 0))
-
-
 dev.off()
 
 
+### SVG HTML
+# doc <- tags$html(
+#   tags$head(
+#     tags$title('My first page')
+#   ),
+#   tags$body(
+#     h1('My first heading'),
+#     p('My first paragraph, with some ',
+#       strong('bold'),
+#       ' text.'),
+#     div(id='myDiv', class='simpleDiv',
+#      suppressMessages(htmlSVG(
+#        slopegraph(df, rescaleByColumn = F, cex.lab = 0.5, cex.num = 0.6, lab.sep = 0.25,
+#                   offset.x = 0.045, offset.lab = 0.03, xlim = c(-1.2, ncol(votes) + 1.2),
+#                   collapse.label = ", ", lwd =  scale(lineW, center = F) / 2, col.lines = cols, mai = c(0.01, 0, 0.9, 0)), height = 10))
+#     )
+#   )
+# )
+# #html_print(doc, background="#dfdada;")
+# save_html(doc, "slopegraph.html")
